7aef2fadab0da24d2d296c7171ce95ab0abba0da
pig002238 <144104624+pig002238@users.noreply.github.com>
2023-10-18 23:05:45 +0900

    目標設定

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,10 +1,25 @@
 class GoalsController < ApplicationController
+  before_action :authenticate_user! # ログインユーザーのみアクセス可
   def index
-   
+    @goals = Goal.all
   end
 
   def new
-    @goals = Goal.new
+    @goal = Goal.new
   end
 
+  def create
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit( :purpose, :goal, :action, :deadline).merge(user_id: current_user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "goals#index"
-  resources :goals, only: [:index, :new]
+  resources :goals, only: [:index, :new, :create]
 end
