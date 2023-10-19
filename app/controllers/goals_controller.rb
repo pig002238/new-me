f0333e92c53b38a112f8c395c75584f20c6aaa6e
pig002238 <144104624+pig002238@users.noreply.github.com>
2023-10-19 18:44:55 +0900

    目標編集、削除ボタン作成

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,7 @@
 class GoalsController < ApplicationController
   before_action :authenticate_user! # ログインユーザーのみアクセス可
+  before_action :set_tweet, only: [:edit, :show, :update, :destroy]
+
   def index
     @goals = Goal.all
   end
@@ -17,9 +19,32 @@ class GoalsController < ApplicationController
     end
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @goal.update(goal_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @goal.destroy
+    redirect_to root_path
+  end
+
   private
 
   def goal_params
     params.require(:goal).permit( :purpose, :goal, :action, :deadline).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @goal = Goal.find(params[:id])
   end
 end
