8e8bbdbfbf439b8baf2ad5e1f7b7ff63b628681b
pig002238 <144104624+pig002238@users.noreply.github.com>
2023-10-24 01:56:36 +0900

    画像投稿追加、ビュー調整

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :goal
   has_one_attached :image
-  
-  validates :content, presence: true
+
+   validates :content, presence: true, unless: :was_attached?
+
+  def was_attached?
+    self.image.attached?
+  end
 end
