044108ed5436cdf4a03bd42f8e92f6a809bed625
pig002238 <144104624+pig002238@users.noreply.github.com>
2023-11-17 23:39:52 +0900

    目標テストコード

--- spec/factories/goals.rb
+++ spec/factories/goals.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :goal do
-    
+    purpose               {Faker::Lorem.paragraph}
+    goal                  {Faker::Lorem.paragraph}
+    action                {Faker::Lorem.paragraph}
+    deadline              {9999/99/99}
+    association :user
   end
 end
--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @goal = FactoryBot.build(:goal)
+  end
+
+  describe "目標新規登録" do
+    context "新規登録できるとき" do
+      it "purposeとgoal、actionとdeadlineが存在すれば登録できる" do
+        expect(@goal).to be_valid
+      end
+    end
+
+    context "新規登録できないとき" do
+      it "purposeが空では登録できない" do
+        @goal.purpose = ""
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include("目的を入力してください")
+      end
+      it "goalが空では登録できない" do
+        @goal.goal = ""
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include("目標を入力してください")
+      end
+      it "actionが空では登録できない" do
+        @goal.action = ""
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include("行動を入力してください")
+      end
+      it "deadlineが空では登録できない" do
+        @goal.deadline = ""
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include("期日を入力してください")
+      end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @goal.user = nil
+        @goal.valid?
+        expect(@goal.errors.full_messages).to include("Userを入力してください")
+      end
+    end
+  end
+
+
 end
