3f70dc6e2d13f4e8d5db786b98284ff79d19b61f
pig002238 <144104624+pig002238@users.noreply.github.com>
2023-12-17 16:44:55 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:index] # ログインユーザーのみアクセス可
-
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
